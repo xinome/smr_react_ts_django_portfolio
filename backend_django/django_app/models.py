4e729f810b24d11df9a7653ff03e9421a2ba3627
xinome <xinome2038@gmail.com>
2025-03-01 18:22:41 +0900

    django admin画面・仮モデル設定＋superuser追加

--- backend_django/django_app/models.py
+++ backend_django/django_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ProjectTopics(models.Model):
+  topic_id = models.AutoField(primary_key=True)
+  date = models.DateField()
+  content = models.CharField(max_length=1000)
+  created_at = models.DateField(auto_now_add=True)
+  updated_at = models.DateField(auto_now=True)
+
+  class Meta:
+    db_table = 'project_topics'
+
+  def __str__(self):
+    return self.content
+    
