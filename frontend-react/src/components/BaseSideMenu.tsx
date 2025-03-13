be2e5a09cecb14360b6cff08a7fad70c0833b653
xinome <xinome2038@gmail.com>
2025-03-13 12:06:50 +0900

    ・Material UI用のスタイルを仮実装 ・インデント等調整

--- frontend-react/src/components/BaseSideMenu.tsx
+++ frontend-react/src/components/BaseSideMenu.tsx
@@ -32,66 +32,66 @@ const BaseSideMenu = () => {
   
   return (
     <Box className='side-menu' style={{ backgroundColor: bgcolor_sidemenu }}>
-          <List>
-            <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen(!isAccordionOpen)}>
-              {/* <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon> */}
-              <ListItemText primary="ポートフォリオ管理" />
-              {isAccordionOpen ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={isAccordionOpen} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <Link to='/portfolio/list/'>
-                  <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
-                    <ListItemText primary="ポートフォリオ一覧" />
-                  </ListItemButton>
-                </Link>
-                <Link to='/portfolio/create/'>
-                  <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
-                    <ListItemText primary="ポートフォリオ作成" />
-                  </ListItemButton>
-                </Link>
-              </List>
-            </Collapse>
-
-            <Link to='/mypage/edit/'>
-              <ListItemButton sx={menuItemStyle}>ユーザ基本情報変更</ListItemButton>
+      <List>
+        <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen(!isAccordionOpen)}>
+          {/* <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon> */}
+          <ListItemText primary="ポートフォリオ管理" />
+          {isAccordionOpen ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={isAccordionOpen} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <Link to='/portfolio/list/'>
+              <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
+                <ListItemText primary="ポートフォリオ一覧" />
+              </ListItemButton>
             </Link>
-
-            <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen2(!isAccordionOpen2)}>
-              <ListItemText primary="プロジェクト管理" />
-              {isAccordionOpen2 ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={isAccordionOpen2} timeout="auto" unmountOnExit>
-              <List component="div" disablePadding>
-                <Link to='/project/list/'>
-                  <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
-                    <ListItemText primary="プロジェクト一覧" />
-                  </ListItemButton>
-                </Link>
-                <Link to='/project/create/'>
-                  <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
-                    <ListItemText primary="プロジェクト作成" />
-                  </ListItemButton>
-                </Link>
-              </List>
-            </Collapse>
-
-            <Link to='/searchproject/'>
-              <ListItemButton sx={menuItemStyle}>プロジェクト検索</ListItemButton>
-            </Link>
-            <Link to='/scout/'>
-              <ListItemButton sx={menuItemStyle}>スカウト管理</ListItemButton>
-            </Link>
-            <Link to='/tips/'>
-              <ListItemButton sx={menuItemStyle}>開発Tips</ListItemButton>
-            </Link>
-            <Link to='/activity/'>
-              <ListItemButton sx={menuItemStyle}>活動記録</ListItemButton>
+            <Link to='/portfolio/create/'>
+              <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
+                <ListItemText primary="ポートフォリオ作成" />
+              </ListItemButton>
             </Link>
           </List>
-        </Box>
+        </Collapse>
+
+        <Link to='/mypage/edit/'>
+          <ListItemButton sx={menuItemStyle}>ユーザ基本情報変更</ListItemButton>
+        </Link>
+
+        <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen2(!isAccordionOpen2)}>
+          <ListItemText primary="プロジェクト管理" />
+          {isAccordionOpen2 ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={isAccordionOpen2} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <Link to='/project/list/'>
+              <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
+                <ListItemText primary="プロジェクト一覧" />
+              </ListItemButton>
+            </Link>
+            <Link to='/project/create/'>
+              <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
+                <ListItemText primary="プロジェクト作成" />
+              </ListItemButton>
+            </Link>
+          </List>
+        </Collapse>
+
+        <Link to='/searchproject/'>
+          <ListItemButton sx={menuItemStyle}>プロジェクト検索</ListItemButton>
+        </Link>
+        <Link to='/scout/'>
+          <ListItemButton sx={menuItemStyle}>スカウト管理</ListItemButton>
+        </Link>
+        <Link to='/tips/'>
+          <ListItemButton sx={menuItemStyle}>開発Tips</ListItemButton>
+        </Link>
+        <Link to='/activity/'>
+          <ListItemButton sx={menuItemStyle}>活動記録</ListItemButton>
+        </Link>
+      </List>
+    </Box>
   );
 }
 
