792239f4cf8e69214bced333d01cb0635b2b0906
xinome <xinome2038@gmail.com>
2025-03-17 14:42:18 +0900

    ・reducers/indexをstore/indexに統合 ・actionTypes削除

--- frontend-react/src/store/index.tsx
+++ frontend-react/src/store/index.tsx
@@ -1,9 +1,30 @@
+import { combineReducers } from 'redux';
+
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 
-import rootReducer from "../reducers/index";
+// Reducers
+import accountReducer from '../features/account/mypageSlice';
+import authReducer from '../features/account/authSlice';
 
-// const initialState = {};
+import projectTopicsReducer from '../features/topics/projectTopicsSlice';
+import portfolioTopicsReducer from '../features/topics/portfolioTopicsSlice';
+import activityTopicsReducer from '../features/topics/activityTopicsSlice';
+
+const rootReducer = combineReducers({
+  // ここに作成したReducerを記述する
+
+  // account
+  accountReducer,
+  authReducer,
+
+  // topics
+  projectTopicsReducer,
+  portfolioTopicsReducer,
+  activityTopicsReducer,
+});
+
+// Store
 const store = configureStore({
   reducer: rootReducer,
 });
