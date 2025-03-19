8f0a5a2b54833bd754443b8e9ad0763def494ca3
xinome <xinome2038@gmail.com>
2025-03-19 14:37:47 +0900

    サイドメニュー: アコーディオン部分の表示フラグ更新

--- frontend-react/src/components/BaseSideMenu.tsx
+++ frontend-react/src/components/BaseSideMenu.tsx
@@ -27,20 +27,38 @@ const BaseSideMenu = () => {
     borderBottom: '1px solid #ccc',
   };
 
-  const [isAccordionOpen, setIsAccordionOpen] = useState<boolean>(false);
-  const [isAccordionOpen2, setIsAccordionOpen2] = useState<boolean>(false);
+  type accordionType = {
+    "portfolio": boolean,
+    "project": boolean,
+    "tips": boolean,
+    [key: string]: boolean; // Add index signature
+  };
+
+  const [isOpenAccordion, setIsOpenAccordion] = useState<accordionType>(
+    {
+      "portfolio": false,
+      "project": false,
+      "tips": false,
+    }
+  );
+
+  const toggleAccordionMenu = (key: string) => {
+    setIsOpenAccordion({ ...isOpenAccordion, [key]: !isOpenAccordion[key] });
+  };
   
   return (
     <Box className='side-menu' style={{ backgroundColor: bgcolor_sidemenu }}>
       <List>
-        <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen(!isAccordionOpen)}>
+        <ListItemButton sx={menuItemStyle}
+          onClick={() => toggleAccordionMenu("portfolio")}
+        >
           {/* <ListItemIcon>
             <InboxIcon />
           </ListItemIcon> */}
           <ListItemText primary="ポートフォリオ管理" />
-          {isAccordionOpen ? <ExpandLess /> : <ExpandMore />}
+          {isOpenAccordion["portfolio"] ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
-        <Collapse in={isAccordionOpen} timeout="auto" unmountOnExit>
+        <Collapse in={isOpenAccordion["portfolio"]} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             <Link to='/portfolio/list/'>
               <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
@@ -59,11 +77,13 @@ const BaseSideMenu = () => {
           <ListItemButton sx={menuItemStyle}>ユーザ基本情報変更</ListItemButton>
         </Link>
 
-        <ListItemButton sx={menuItemStyle} onClick={() => setIsAccordionOpen2(!isAccordionOpen2)}>
+        <ListItemButton sx={menuItemStyle}
+          onClick={() => toggleAccordionMenu("project")}
+        >
           <ListItemText primary="プロジェクト管理" />
-          {isAccordionOpen2 ? <ExpandLess /> : <ExpandMore />}
+          {isOpenAccordion["project"] ? <ExpandLess /> : <ExpandMore />}
         </ListItemButton>
-        <Collapse in={isAccordionOpen2} timeout="auto" unmountOnExit>
+        <Collapse in={isOpenAccordion["project"]} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
             <Link to='/project/list/'>
               <ListItemButton sx={{ ...menuItemStyle, pl: 4 }}>
@@ -84,9 +104,26 @@ const BaseSideMenu = () => {
         <Link to='/scout/'>
           <ListItemButton sx={menuItemStyle}>スカウト管理</ListItemButton>
         </Link>
-        <Link to='/tips/'>
-          <ListItemButton sx={menuItemStyle}>開発Tips</ListItemButton>
-        </Link>
+        <ListItemButton sx={menuItemStyle}
+          onClick={() => toggleAccordionMenu("tips")}
+        >
+          <ListItemText primary="開発tips" />
+          {isOpenAccordion["tips"] ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={isOpenAccordion["tips"]} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <Link to='/tips/list/'>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="開発tips一覧" />
+              </ListItemButton>
+            </Link>
+            <Link to='/tips/create/'>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemText primary="開発tips作成" />
+              </ListItemButton>
+            </Link>
+          </List>
+        </Collapse>
         <Link to='/activity/'>
           <ListItemButton sx={menuItemStyle}>活動記録</ListItemButton>
         </Link>
